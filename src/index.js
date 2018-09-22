c50868cf67c37f06c0a2cc0a373253b72eb32cf6
Mariya <mariya.poltoratskaya@gmail.com>
2018-09-23 01:06:42 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var zerosCount = 0;
+  var i = 0;
+  for (var n = 5; n <= number; n *= 5) {
+    for (i = n; i <= number; i += n) {
+      zerosCount++;
+    }
+  }
+  return zerosCount;
 }

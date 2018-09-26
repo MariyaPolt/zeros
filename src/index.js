d792b6dbe625d6c7885cef3c47ec1f76a30183a1
Mariya <37655379+MariyaPolt@users.noreply.github.com>
2018-09-27 00:11:41 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = function getZerosCount(number) {
-  var zerosCount = 0;
-  for (var n = 5; n <= number; n *= 5) {
-    for (var i = n; i <= number; i += n) {
+  let zerosCount = 0;
+  for (let n = 5; n <= number; n *= 5) {
+    for (let i = n; i <= number; i += n) {
       zerosCount++;
     }
   }
